9405f53e4d4aa219f8135c5657526778fb21f32d
kore002 <59078274+kore002@users.noreply.github.com>
2022-09-02 14:38:23 +0900

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="login">
+        <h1>{{ msg }}</h1>
+        <h2>로그인 기능</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'login sample'
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+ font-weight: normal;
+}
+</style>
