36436c95628362f87a302467c3395f067bafc2e0
kore002 <59078274+kore002@users.noreply.github.com>
2022-09-05 14:07:31 +0900

    이거 안할거

---
+++ src/components/TextInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="login">
+        <h1>{{ msg }}</h1>
+        <h2>로그인 기능</h2>
+        <label>제목 : </label><input type="text"/><br/>
+        <label>내용</label><br/><input type="textarea"/>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'textinput',
+  data () {
+    return {
+      msg: 'login sample'
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+ font-weight: normal;
+}
+</style>
